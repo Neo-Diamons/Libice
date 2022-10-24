91e969ceec35f866abea1b8c574c4904a5c73fbd
Romain <romain.hure@epitech.eu>
2022-10-24 11:52:16 +0200

    Improve ice_sint to work with negative numbers and when nb = 10^n.

--- lib/ice/ice_sint.c
+++ lib/ice/ice_sint.c
@@ -11,18 +11,23 @@
 char *ice_sint(int nb)
 {
     int i = 1;
+    int neg = 0;
     int len = 0;
     char *str = 0;
-
-    for (; i < nb ; i *= 10) {
-        len++;
+    if (nb < 0) {
+        nb *= -1;
+        neg++;
     }
-    str = malloc(sizeof(char) * len);
+    for (; i <= nb ; i *= 10)
+        len++;
+    str = malloc(sizeof(char) * (len + neg));
     str[i] = '\0';
     for (i = len - 1 ; 0 <= i ; i--) {
-        str[i] = 48 + nb % 10;
+        str[i + neg] = 48 + nb % 10;
         nb /= 10;
     }
-
+    if (neg) {
+        str[0] = '-';
+    }
     return str;
 }
--- tests/test_ice_sint.c
+++ tests/test_ice_sint.c
@@ -2,5 +2,9 @@
 #include <ice.h>
 
 Test (ice_sint, sint) {
-    cr_assert_str_eq(ice_sint(89), "89");
+    cr_assert_str_eq(ice_sint(-89), "-89");
+}
+
+Test (ice_sint, sint_10) {
+    cr_assert_str_eq(ice_sint(10), "10");
 }
