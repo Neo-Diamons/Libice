f1c73ffb13586f80b51ad40241fb023c7847b18e
diamons <romain.hure@epitech.eu>
2023-02-07 16:25:06 +0100

    FEAT(<test_string.c>): add test for <ice_strcpy.c>, <ice_strncpy.c>, <ice_strcat.c> and <ice_strncat.c>.

--- tests/test_string.c
+++ tests/test_string.c
@@ -22,3 +22,41 @@ Test(ice_strlen, empty) {
 
     cr_assert_eq(len, 0, "ice_strlen() returned %lli instead of 0", len);
 }
+
+Test(ice_strcpy, basic) {
+    char str1[15] = {0};
+    char str2[] = "Hello World!";
+
+    ice_strcpy(str1, str2);
+
+    cr_assert_str_eq(str1, str2, "ice_strcpy() returned %s instead of Hello World!", str1);
+}
+
+Test(ice_strncpy, basic) {
+    char str1[15] = {0};
+    char str2[] = "Hello World!";
+
+    ice_strncpy(str1, str2, 5);
+
+    cr_assert_str_eq(str1, "Hello", "ice_strncpy() returned %s instead of Hello World!", str1);
+}
+
+Test(ice_strcat, basic) {
+    char str1[15] = {0};
+    char str2[] = " World!";
+
+    ice_strcpy(str1, "Hello");
+    ice_strcat(str1, str2);
+
+    cr_assert_str_eq(str1, "Hello World!", "ice_strcat() returned %s instead of Hello World!", str1);
+}
+
+Test(ice_strncat, basic) {
+    char str1[15] = {0};
+    char str2[] = " World!";
+
+    ice_strcpy(str1, "Hello");
+    ice_strncat(str1, str2, 6);
+
+    cr_assert_str_eq(str1, "Hello World", "ice_strncat() returned %s instead of Hello World!", str1);
+}
