f1731b83512418898ce9ad68d6f48dfbda68f8b4
diamons <romain.hure@epitech.eu>
2023-02-13 10:18:20 +0100

    FEAT(<test_list.c>): add test for <list_get.c>.

--- tests/test_list.c
+++ tests/test_list.c
@@ -82,3 +82,40 @@ Test(list_destroy_node, destroy_list_and_node) {
 
     list_destroy_node(list, free);
 }
+
+Test(list_get, get_node) {
+    bool r;
+    void *element;
+    list_t *list = list_create();
+    cr_assert_neq(list, NULL, "list_create() returned NULL");
+
+    r = list_add(list, "Hello");
+    cr_assert_eq(r, false, "list_add() returned true");
+
+    r = list_add(list, "World!");
+    cr_assert_eq(r, false, "list_add() returned true");
+
+    element = list_get(list, 1);
+
+    cr_assert_str_eq(element, list->tail->value, "element is not list->tail->value");
+
+    list_destroy(list);
+}
+
+Test(list_get, get_node_overflow_index) {
+    bool r;
+    void *element;
+    list_t *list = list_create();
+    cr_assert_neq(list, NULL, "list_create() returned NULL");
+
+    r = list_add(list, "Hello");
+    cr_assert_eq(r, false, "list_add() returned true");
+
+    r = list_add(list, "World!");
+    cr_assert_eq(r, false, "list_add() returned true");
+
+    element = list_get(list, 3);
+    cr_assert_eq(element, NULL, "element is not NULL");
+
+    list_destroy(list);
+}
