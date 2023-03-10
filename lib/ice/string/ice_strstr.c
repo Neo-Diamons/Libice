94dfb59f681277e10a9264fd8ab6e236b9505503
diamons <romain.hure@epitech.eu>
2023-03-10 01:22:30 +0100

    STYLE(<ice_strstr.c>): remove major.

--- lib/ice/string/ice_strstr.c
+++ lib/ice/string/ice_strstr.c
@@ -14,8 +14,11 @@ char *ice_strstr(const char *haystack, const char *needle)
     for (ull_t i = 0, j = 0 ; haystack[i] ; i++) {
         if (haystack[i] == needle[j]) {
             j++;
-            if (!needle[j])
-                return ((char *)haystack + i - j + 1);
+            (!needle[j]) ?
+            ({
+                return ((char *) haystack + i - j + 1);
+            }) : (0);
+
         } else
             j = 0;
     }
